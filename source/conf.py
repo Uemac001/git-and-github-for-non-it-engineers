50aab743ab1d4f827d6de873cfddfe2b52f223bb
starfieldsKt <106499621+starfieldKt@users.noreply.github.com>
2024-03-19 16:10:47 +0900

    チュートリアル追加

--- source/conf.py
+++ source/conf.py
@@ -39,4 +39,8 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
 
+html_theme_options = {
+    "navigation_depth": 5,
+}
+
 html_scaled_image_link = True
